9c32424d9446631bb4f90619678f43af6fa2622e
gembeconsulting <github@gembe-consulting.de>
2017-10-14 13:56:41 +0200

    Initial Commit of existing files
    All files from project re-comitted to github

---
+++ src/mii/util/MIIJSONModel.js
@@ -0,0 +1,60 @@
+/**
+ * JSON-based DataBinding for MII Services
+ */
+
+// Provides the JSON object based model implementation
+sap.ui.define(["jquery.sap.global", "sap/ui/model/json/JSONModel"],
+	function(jQuery, JSONModel) {
+		"use strict";
+
+		/**
+		 * Constructor for a new MIIModel.
+		 *
+		 * @class
+		 * Model implementation for JSON based MII Services
+		 *
+		 * @extends sap.ui.model.JSONModel
+		 *
+		 * @author Philipp Gembe
+		 * @version 0.1
+		 *
+		 * @param {object} oData either the URL where to load the JSON from or a JS object
+		 * @param {object} oURLParameters
+		 * @public
+		 */
+		var MIIJSONModel = JSONModel.extend("mii.util.MIIJSONModel", /** @lends mii.util.MIIJSONModel.prototype */ {
+			constructor: function(sUrl, oURLParameters) {
+				if (!sUrl || typeof sUrl !== "string") {
+					jQuery.sap.log.error("Parameter sUrl must be type of string", null, this.toString());
+					return;
+				}
+				JSONModel.apply(this, arguments);
+				
+				this._sUrl = sUrl;
+			},
+
+			metadata: {
+				publicMethods: ["setMiiData", "getMiiData"]
+			}
+		});
+
+		/**
+		 * Override JSON Models loadData method, to only load data, if oURLParameters is given. 
+		 * This is to ensure having proper input data for MII QueryTemplate
+		 * @public
+		 */
+		MIIJSONModel.prototype.loadData = function(sUrl, oURLParameters, bAsync, sType, bMerge, bCache, mHeaders) {
+			
+			if (sUrl && typeof sUrl === "string" && oURLParameters) {
+				
+				JSONModel.load(sUrl, oURLParameters, bAsync, sType, bMerge, bCache, mHeaders);
+
+			} else {
+				jQuery.sap.log.warning("Method loadData() is missing oURLParameters. Data not loaded.", null, this.toString());
+			}
+
+		};
+		
+		return MIIJSONModel;
+
+	});
